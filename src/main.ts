37a5e4388b44652aca8d745167f198b2ef86740c
jwvonk <jwavonk@gmail.com>
2023-11-10 11:34:19 -0800

    Part C Done

--- src/main.ts
+++ src/main.ts
@@ -42,120 +42,198 @@ sensorButton.addEventListener("click", () => {
     playerMarker.setLatLng(
       leaflet.latLng(position.coords.latitude, position.coords.longitude)
     );
-    map.setView(playerMarker.getLatLng());
+    generate();
   });
 });
 
-const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
-statusPanel.innerHTML = "No coins yet...";
+const northButton = document.querySelector("#north")!;
+northButton.addEventListener("click", () => {
+  const current = playerMarker.getLatLng();
+  playerMarker.setLatLng(
+    leaflet.latLng(current.lat + TILE_DEGREES, current.lng)
+  );
+  generate();
+});
 
-class Coin {
+const southButton = document.querySelector("#south")!;
+southButton.addEventListener("click", () => {
+  const current = playerMarker.getLatLng();
+  playerMarker.setLatLng(
+    leaflet.latLng(current.lat - TILE_DEGREES, current.lng)
+  );
+  generate();
+});
+
+const eastButton = document.querySelector("#east")!;
+eastButton.addEventListener("click", () => {
+  const current = playerMarker.getLatLng();
+  playerMarker.setLatLng(
+    leaflet.latLng(current.lat, current.lng + TILE_DEGREES)
+  );
+  generate();
+});
+
+const westButton = document.querySelector("#west")!;
+westButton.addEventListener("click", () => {
+  const current = playerMarker.getLatLng();
+  playerMarker.setLatLng(
+    leaflet.latLng(current.lat, current.lng - TILE_DEGREES)
+  );
+  generate();
+});
+
+interface Coin {
   readonly i: number;
   readonly j: number;
   readonly serial: number;
+}
 
-  constructor(i: number, j: number, serial: number) {
+class Geocache implements Momento<string> {
+  i: number;
+  j: number;
+  coins: Coin[] = [];
+
+  constructor(i: number, j: number) {
     this.i = i;
     this.j = j;
-    this.serial = serial;
   }
 
-  get displayText() {
-    return `${this.i}:${this.j}#${this.serial}`;
+  gennerateCoins() {
+    this.coins = new Array<Coin>(
+      Math.floor(luck([this.i, this.j, "initialValue"].toString()) * 100)
+    );
+    for (let serial = 0; serial < this.coins.length; serial++) {
+      this.coins[serial] = { i: this.i, j: this.j, serial };
+    }
   }
 
-  get id() {
-    return `id${this.i}_${this.j}_${this.serial}`;
+  get momentoKey() {
+    return `${this.i},${this.j}`;
   }
+
+  toMomento() {
+    return JSON.stringify(this.coins);
+  }
+
+  fromMomento(momento: string) {
+    this.coins = JSON.parse(momento) as Coin[];
+  }
+}
+
+interface Momento<T> {
+  toMomento(): T;
+  fromMomento(momento: T): void;
 }
 
 const board = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
 
 const playerCoins: Coin[] = [];
 
-const cacheCoinRegistry = new Map<string, Coin[]>();
+const cacheMomentos = new Map<string, string>();
+
+const cahceLayers = leaflet.layerGroup().addTo(map);
 
 function makeCache(i: number, j: number) {
   const bounds = board.getCellBounds({ i, j });
 
-  const geoCache = leaflet.rectangle(bounds) as leaflet.Layer;
+  const cacheLayer = leaflet.rectangle(bounds) as leaflet.Layer;
 
-  const cacheKey = `${i},${j}`;
-  let cacheCoins: Coin[];
-  if (!cacheCoinRegistry.has(cacheKey)) {
-    cacheCoins = new Array<Coin>(
-      Math.floor(luck([i, j, "initialValue"].toString()) * 100)
-    );
-    for (let serial = 0; serial < cacheCoins.length; serial++) {
-      cacheCoins[serial] = new Coin(i, j, serial);
-    }
-    cacheCoinRegistry.set(cacheKey, cacheCoins);
+  const cache = new Geocache(i, j);
+
+  if (!cacheMomentos.has(cache.momentoKey)) {
+    cache.gennerateCoins();
+    cacheMomentos.set(cache.momentoKey, cache.toMomento());
   } else {
-    cacheCoins = cacheCoinRegistry.get(cacheKey)!;
+    cache.fromMomento(cacheMomentos.get(cache.momentoKey)!);
+  }
+
+  function displayText(coin: Coin) {
+    return `${coin.i}:${coin.j}#${coin.serial}`;
+  }
+
+  function id(coin: Coin) {
+    return `id${coin.i}_${coin.j}_${coin.serial}`;
   }
 
   function hideCoinHTML(elem: HTMLDivElement, coin: Coin) {
-    elem.querySelector<HTMLLIElement>(`#${coin.id}`)!.classList.add("hidden");
+    elem.querySelector<HTMLLIElement>(`#${id(coin)}`)!.classList.add("hidden");
   }
 
   function revealCoinHTML(elem: HTMLDivElement, coin: Coin) {
     elem
-      .querySelector<HTMLLIElement>(`#${coin.id}`)!
+      .querySelector<HTMLLIElement>(`#${id(coin)}`)!
       .classList.remove("hidden");
   }
 
-  geoCache.bindPopup(() => {
-    const container = document.createElement("div");
-    container.style.display = "flex";
-    container.style.alignContent = "space-around";
-    container.innerHTML += `<ul id="playerCoins">(Player Coins)</ul><hr><ul id="cacheCoins">(Cache Coins)</ul>`;
+  cacheLayer.bindPopup(
+    () => {
+      const container = document.createElement("div");
+      container.style.display = "flex";
+      container.style.alignContent = "space-around";
+      container.innerHTML += `<ul id="playerCoins">(Player Coins)</ul><hr><ul id="cacheCoins">(Cache Coins)</ul>`;
 
-    const playerCoinsElem =
-      container.querySelector<HTMLDivElement>("#playerCoins")!;
-    const cacheCoinsElem =
-      container.querySelector<HTMLDivElement>("#cacheCoins")!;
+      const playerCoinsElem =
+        container.querySelector<HTMLDivElement>("#playerCoins")!;
+      const cacheCoinsElem =
+        container.querySelector<HTMLDivElement>("#cacheCoins")!;
 
-    const allCoins = playerCoins.concat(cacheCoins);
+      const allCoins = playerCoins.concat(cache.coins);
 
-    for (const coin of allCoins) {
-      playerCoinsElem.innerHTML += `<li id=${coin.id}><span>${coin.displayText}</span><button id=${coin.id}>></button></li>`;
-      cacheCoinsElem.innerHTML += `<li id=${coin.id}><button id=${coin.id}><</button><span>\t${coin.displayText}</span></li>`;
-    }
+      for (const coin of allCoins) {
+        playerCoinsElem.innerHTML += `<li id=${id(coin)}><span>${displayText(
+          coin
+        )}\t</span><button id=${id(coin)}>></button></li>`;
+        cacheCoinsElem.innerHTML += `<li id=${id(coin)}><button id=${id(
+          coin
+        )}><</button><span>\t${displayText(coin)}</span></li>`;
+      }
 
-    for (const coin of playerCoins) {
-      hideCoinHTML(cacheCoinsElem, coin);
-    }
-
-    for (const coin of cacheCoins) {
-      hideCoinHTML(playerCoinsElem, coin);
-    }
-
-    for (const coin of allCoins) {
-      let button = playerCoinsElem.querySelector<HTMLButtonElement>(
-        `#${coin.id}`
-      )!;
-      button.addEventListener("click", () => {
-        cacheCoins.push(playerCoins.splice(playerCoins.indexOf(coin), 1)[0]);
-        hideCoinHTML(playerCoinsElem, coin);
-        revealCoinHTML(cacheCoinsElem, coin);
-      });
-
-      button = cacheCoinsElem.querySelector<HTMLButtonElement>(`#${coin.id}`)!;
-      button.addEventListener("click", () => {
-        playerCoins.push(cacheCoins.splice(cacheCoins.indexOf(coin), 1)[0]);
+      for (const coin of playerCoins) {
         hideCoinHTML(cacheCoinsElem, coin);
-        revealCoinHTML(playerCoinsElem, coin);
-      });
-    }
-    return container;
-  });
-  geoCache.addTo(map);
+      }
+
+      for (const coin of cache.coins) {
+        hideCoinHTML(playerCoinsElem, coin);
+      }
+
+      for (const coin of allCoins) {
+        let button = playerCoinsElem.querySelector<HTMLButtonElement>(
+          `#${id(coin)}`
+        )!;
+        button.addEventListener("click", () => {
+          cache.coins.push(playerCoins.splice(playerCoins.indexOf(coin), 1)[0]);
+          cacheMomentos.set(cache.momentoKey, cache.toMomento());
+          hideCoinHTML(playerCoinsElem, coin);
+          revealCoinHTML(cacheCoinsElem, coin);
+        });
+
+        button = cacheCoinsElem.querySelector<HTMLButtonElement>(
+          `#${id(coin)}`
+        )!;
+        button.addEventListener("click", () => {
+          playerCoins.push(cache.coins.splice(cache.coins.indexOf(coin), 1)[0]);
+          cacheMomentos.set(cache.momentoKey, cache.toMomento());
+          hideCoinHTML(cacheCoinsElem, coin);
+          revealCoinHTML(playerCoinsElem, coin);
+        });
+      }
+      return container;
+    },
+    { maxWidth: 500 }
+  );
+  cahceLayers.addLayer(cacheLayer);
 }
 
-const currentCells = board.getCellsNearPoint(NULL_ISLAND);
+function generate() {
+  cahceLayers.clearLayers();
+  map.setView(playerMarker.getLatLng());
+  const currentCells = board.getCellsNearPoint(playerMarker.getLatLng());
 
-for (const cell of currentCells) {
-  if (luck([cell.i, cell.j].toString()) < PIT_SPAWN_PROBABILITY) {
-    makeCache(cell.i, cell.j);
+  for (const cell of currentCells) {
+    if (luck([cell.i, cell.j].toString()) < PIT_SPAWN_PROBABILITY) {
+      makeCache(cell.i, cell.j);
+    }
   }
 }
+
+generate();
